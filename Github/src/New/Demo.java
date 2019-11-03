0099f8a7bfe56b287da8b9e6ff4d40b032fe17fb
aswin <aswin@DESKTOP-VVUHMS0>
2019-11-03 14:52:46 +0530

    c

---
+++ Github/src/New/Demo.java
@@ -0,0 +1,15 @@
+package New;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("hello", true);
+		
+	}
+
+}
